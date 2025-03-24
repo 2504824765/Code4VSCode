e9621560dabf48df6d090416bb84a61743493157
2504824765 <cz2504824765@outlook.com>
2025-03-24 09:24:14 +0800

    second commit

---
+++ Study/js/helloworld.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+var x;
+var y = 5;
+const pi = 3.14;
+
+x = 10;
+
+console.log(x);
+console.log(y);
+console.log(pi);
+
+if (x > y) {
+    console.log("x is greater than y");
+}
+else {
+    console.log("x is less than or equal to y");
+}

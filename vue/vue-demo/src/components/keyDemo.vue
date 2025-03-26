809eb7bebc247af4d43cacf33948af0703745ba7
2504824765 <cz2504824765@outlook.com>
2025-03-26 16:29:20 +0800

    Finish KeyDemo

--- vue/vue-demo/src/components/keyDemo.vue
+++ vue/vue-demo/src/components/keyDemo.vue
@@ -1,3 +1,19 @@
 <template>
     <h3>Key属性添加到v-for中</h3>
+    <p v-for="(item, index) in results" :key="item.id">{{ item }}</p>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                names: ['张三', '李四', '王五'],
+                results: [
+                    { id: 1, name: '张三' },
+                    { id: 2, name: '李四' },
+                    { id: 3, name: '王五' }
+                ],
+            }
+        }
+    }
+</script>
